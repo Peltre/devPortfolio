04b83ca5118ae502bec5cebeb7e9ea8d7fabeb2a
Peltre <pedripjs@gmail.com>
2026-04-28 21:58:10 -0600

    Initial commit
    Initialize base project structure

---
+++ App.jsx
@@ -0,0 +1,11 @@
+
+
+
+
+function App () {
+    return (
+        <p>This is the initial commit</p>
+    )
+}
+
+export default App
---
+++ README.md
@@ -0,0 +1,1 @@
+# Hello my name is Pedro & this is my Developer Portfolio
