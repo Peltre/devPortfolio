8240ee48322b49d775a90190a310a993aedde4ea
Peltre <pedripjs@gmail.com>
2026-04-30 23:50:16 -0600

    Add basic app layout & apply gradient bg (TBD)

--- App.jsx
+++ App.jsx
@@ -4,7 +4,12 @@ import Navbar from "./src/components/Navbar"
 
 function App () {
     return (
-        <Navbar/>
+        <div className="min-h-screen bg-linear-to-br from-neutral-950 via-slate-900 to-neutral-950">
+            <Navbar/>
+            <div>
+                
+            </div>
+        </div>
     )
 }
 
