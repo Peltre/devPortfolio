d2377ad9c6567e2c7c24c0038f435a68f62f4443
Peltre <pedripjs@gmail.com>
2026-05-02 00:09:55 -0600

    update the website's background to a more-tech oriented one

--- App.jsx
+++ App.jsx
@@ -5,9 +5,12 @@ import Navbar from "./src/components/Navbar"
 
 function App () {
     return (
-        <div className="min-h-screen bg-gray-900">
-            <Navbar/>
-            <MCover/>
+        <div className="min-h-screen bg-gray-950 relative">
+            <div className="absolute inset-0 bg-[radial-gradient(circle,rgba(255,255,255,0.03)_1px,transparent_1px)] bg-size-[32px_32px]" />
+            <div className="relative z-10">
+                <Navbar />
+                <MCover/>
+            </div>
         </div>
     )
 }
