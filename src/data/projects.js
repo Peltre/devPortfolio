82b44dd15c350638023c6927b9cdf2dd49dd5726
Pedro Sotelo <pedripjs@gmail.com>
2026-05-03 18:06:00 -0600

    Create projects.js
    This file will contain in an array of objects, the projects ive done, with everything I need to display in my component

---
+++ src/data/projects.js
@@ -0,0 +1,24 @@
+export const projects = [
+    {
+        id: 1,
+        title: "Dummy",
+        date: "2024",
+        role: "Fullstack Developer",
+        description: "Modern web app built using react & node js",
+        image: "/projects/proyecto1.jpg",
+        github: "link al gitirub",
+        demo: "link al demo",
+        tags: ["React", "Node.js", "MongoDB"]
+    }, 
+    {
+    id: 2,
+    title: "Mi Videojuego",
+    date: "2023",
+    role: "Game Developer",
+    description: "Juego 2D desarrollado en Unity. Mecánicas de plataformas con temática de dinosaurios.",
+    image: "/projects/proyecto2.jpg",
+    github: "https://github.com/Peltre/proyecto2",
+    demo: "https://pedr1p.itch.io/",
+    tags: ["Unity", "C#", "Game Design"]
+  },
+]
