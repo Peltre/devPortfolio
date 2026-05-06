30a8adef22e519541a26857be932c414a39f4921
Pedro Sotelo <pedripjs@gmail.com>
2026-05-05 22:14:32 -0600

    Update projects structure for each project to contain an array of images for the modal to display

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,22 +1,30 @@
 export const projects = [
-    {
-        id: 1,
-        title: "Dummy",
-        date: "2024",
-        role: "Fullstack Developer",
-        description: "Modern web app built using react & node js",
-        image: "/projects/proyecto1.jpg",
-        github: "link al gitirub",
-        demo: "link al demo",
-        tags: ["React", "Node.js", "MongoDB"]
-    }, 
-    {
+  {
+    id: 1,
+    title: "Dummy",
+    date: "2024",
+    role: "Fullstack Developer",
+    description: "Modern web app built using react & node js",
+    images: [
+      "/projects/proyecto1-1.jpg",
+      "/projects/proyecto1-2.jpg",
+      "/projects/proyecto1-3.jpg",
+    ],
+    github: "link al gitirub",
+    demo: "link al demo",
+    tags: ["React", "Node.js", "MongoDB"]
+  },
+  {
     id: 2,
     title: "Mi Videojuego",
     date: "2023",
     role: "Game Developer",
     description: "Juego 2D desarrollado en Unity. Mecánicas de plataformas con temática de dinosaurios.",
-    image: "/projects/proyecto2.jpg",
+    images: [
+      "/projects/proyecto2-1.jpg",
+      "/projects/proyecto2-2.jpg",
+      "/projects/proyecto2-3.jpg",
+    ],
     github: "https://github.com/Peltre/proyecto2",
     demo: "https://pedr1p.itch.io/",
     tags: ["Unity", "C#", "Game Design"]
