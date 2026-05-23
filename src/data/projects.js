c9f424b921d7d5c7b263b1bd5f1faf9ba80f9418
Pedro Sotelo <pedripjs@gmail.com>
2026-05-22 23:02:26 -0600

    Add images & actual project info

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,32 +1,58 @@
+import topcut1 from "../assets/projects/topcut1.png"
+import topcut2 from "../assets/projects/topcut2.png"
+import topcut3 from "../assets/projects/topcut3.png"
+import phlox1 from "../assets/projects/phlox1.png"
+import phlox2 from "../assets/projects/phlox2.png"
+import phlox3 from "../assets/projects/phlox3.png"
+import kod1 from "../assets/projects/kod1.png"
+import kod2 from "../assets/projects/kod2.png"
+import kod3 from "../assets/projects/kod3.png"
+
+
 export const projects = [
   {
     id: 1,
-    title: "Dummy",
-    date: "2024",
+    title: "TopCut",
+    date: "2026",
     role: "Fullstack Developer",
-    description: "Modern web app built using react & node js",
+    description: "Modern movie & series rating duel game created with React + TMDB API",
     images: [
-      "/projects/proyecto1-1.jpg",
-      "/projects/proyecto1-2.jpg",
-      "/projects/proyecto1-3.jpg",
+      topcut1,
+      topcut2,
+      topcut3,
     ],
-    github: "link al gitirub",
-    demo: "link al demo",
-    tags: ["React", "Node.js", "MongoDB"]
+    github: "https://github.com/Peltre/TopCut",
+    demo: "https://top-cut-three.vercel.app/",
+    tags: ["React", "Tailwind", "Vite"]
   },
   {
     id: 2,
-    title: "Mi Videojuego",
-    date: "2023",
-    role: "Game Developer",
-    description: "Juego 2D desarrollado en Unity. Mecánicas de plataformas con temática de dinosaurios.",
+    title: "Phlox",
+    date: "2025",
+    role: "Scrum Master & Full-Stack Developer",
+    description: "Project Management WebApp that utilizes Softtek's AI called Frida",
     images: [
-      "/projects/proyecto2-1.jpg",
-      "/projects/proyecto2-2.jpg",
-      "/projects/proyecto2-3.jpg",
+      phlox1,
+      phlox2,
+      phlox3,
     ],
-    github: "https://github.com/Peltre/proyecto2",
-    demo: "https://pedr1p.itch.io/",
+    github: "private",
+    demo: "private",
+    tags: ["React", "Unity", "Node.js"]
+  },
+  {
+    id: 3,
+    title: "Knights Of Dango",
+    date: "2024",
+    role: "Solo Developer",
+    description: "2D Plataformer developed in 1 week for a class game jam, created using Unity",
+    images: [
+      kod1,
+      kod2,
+      kod3,
+    ],
+    github: "no hay",
+    demo: "https://pedr1p.itch.io/knights-of-dango",
     tags: ["Unity", "C#", "Game Design"]
   },
 ]
