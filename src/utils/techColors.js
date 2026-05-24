2d61cbabd3b778ce2bc334eb9e2935e8db0a1307
Pedro Sotelo <pedripjs@gmail.com>
2026-05-23 23:06:43 -0600

    Implement techColors utility file to easily control color matching tool

---
+++ src/utils/techColors.js
@@ -0,0 +1,11 @@
+export const techColors = {
+    JavaScript: "#f7df1e",
+    React: "#61dafb",
+    "Node.js": "#6ee7b7",
+    Python: "#60a5fa",
+    Unity: "#e2e8f0",
+    "C#": "#a78bfa",
+    Figma: "#f472b6",
+    Git: "#fb923c",
+    Tailwind: "#38bdf8",
+}
